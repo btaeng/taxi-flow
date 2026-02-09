d3bf51b5db41a8b3cb9d0d75550ed3b685add43b
Bruce Tang <btange7@gmail.com>
2026-02-09 13:37:27 -0800

    dark mode makes it hard to see anything

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,7 +52,10 @@ function App() {
       </div>
 
       <MapContainer center={[40.7580, -73.9857]} zoom={13} style={{ height: "100vh" }}>
-        <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
+        <TileLayer 
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
         <MapClickHandler />
         {drivers.map(d => (
           <Marker key={d.id} position={[d.lat, d.lng]} icon={carIcon}>
